fb80a62685c5357d0a76452681f596b1c9fa28b6
sashacmc <Alexander@Bushnev.pro>
2024-04-23 22:47:15 +0200

    Move video concatenation in separate module, add different progress states

--- datelib.py
+++ datelib.py
@@ -6,6 +6,11 @@ import logging
 import audiolib
 import sqlite3
 
+PROCESSED_NONE = 0
+PROCESSED_IN_PROCESS = 1
+PROCESSED_CONVERTED = 2
+PROCESSED_UPLOADED = 3
+
 SCHEMA = '''
 CREATE TABLE IF NOT EXISTS files (
     "date" TEXT,
@@ -25,7 +30,17 @@ CREATE UNIQUE INDEX IF NOT EXISTS dates_date ON dates (date);
 
 
 class DateLib(object):
-    def __init__(self, scan_paths, db_file):
+    def __init__(self, scan_paths=None, db_file=None):
+        if scan_paths is None:
+            scan_paths = list(
+                filter(
+                    None,
+                    os.getenv("VIDEO_LIB_ROOTS").split(":"),
+                ),
+            )
+        if db_file is None:
+            db_file = os.getenv("VIDEO_LIB_DB")
+
         self.__conn = sqlite3.connect(os.path.expanduser(db_file))
         self.__conn.executescript(SCHEMA)
 
@@ -57,33 +72,70 @@ class DateLib(object):
             c.execute(
                 "INSERT \
                  INTO dates (date, processed) \
-                 VALUES (?, False) \
+                 VALUES (?, ?) \
                  ON CONFLICT(date) \
                  DO UPDATE \
-                 SET processed=False",
-                (date,),
+                 SET processed=?",
+                (date, PROCESSED_NONE, PROCESSED_NONE),
             )
             c.execute("commit")
         except Exception:
             c.execute("rollback")
             logging.exception("add_file failed")
 
-    def set_processed(self, date, url):
+    def __set_processed(self, date, processed, url):
         c = self.__conn.cursor()
-        c.execute(
-            "UPDATE dates \
-             SET processed=True, url=? \
-             WHERE date=?",
-            (url, date),
-        )
-        return c.rowcount == 1
+        c.execute("begin")
+        try:
+            c = self.__conn.cursor()
+            res = c.execute(
+                "SELECT processed FROM dates \
+                                  WHERE date=? ",
+                (date,),
+            )
+            res = res.fetchall()
+            if len(res) != 1:
+                raise Exception(f"checks processed failed: {res}")
+            logging.info(res)
+            if res[0][0] == processed:
+                c.execute("rollback")
+                return False
+
+            c.execute(
+                "UPDATE dates \
+                 SET processed=?, url=? \
+                 WHERE date=?",
+                (processed, url, date),
+            )
+            if c.rowcount != 1:
+                raise Exception(f"set processed failed: {c.rowcount}")
+
+            c.execute("commit")
+            return True
+        except Exception:
+            c.execute("rollback")
+            logging.exception("__set_processed failed")
+            raise
+
+    def set_not_processed(self, date):
+        return self.__set_processed(date, PROCESSED_NONE, None)
+
+    def set_in_progress(self, date):
+        return self.__set_processed(date, PROCESSED_IN_PROCESS, None)
+
+    def set_converted(self, date):
+        return self.__set_processed(date, PROCESSED_CONVERTED, None)
+
+    def set_uploaded(self, date, url):
+        return self.__set_processed(date, PROCESSED_UPLOADED, url)
 
     def get_nonprocessed(self):
         c = self.__conn.cursor()
         res = c.execute(
             "SELECT date, url FROM dates \
-                              WHERE processed=False \
+                              WHERE processed=? \
                               ORDER BY date",
+            (PROCESSED_NONE,),
         )
         return res.fetchall()
 
@@ -115,14 +167,7 @@ class DateLib(object):
 
 def main():
     log.initLogger(level=logging.DEBUG)
-    scan_paths = list(
-        filter(
-            None,
-            os.getenv("VIDEO_LIB_ROOTS").split(":"),
-        ),
-    )
-    db_file = os.getenv("VIDEO_LIB_DB")
-    lib = DateLib(scan_paths, db_file)
+    lib = DateLib()
     lib.scan()
     # print(lib.set_processed("2012-01-20", "some_url"))
     # for date, url in lib.get_nonprocessed():
--- video.py
+++ video.py
@@ -6,216 +6,12 @@ import progressbar
 import logging
 import argparse
 import datelib
-import subprocess
-import json
+
+import mixvideoconcat
 
 import googleapiclient.discovery
 
 YOUTUBE_MAX_DESCRIPTION = 5000
-
-
-def ffprobe_get_video_info(filename):
-    command = [
-        'ffprobe',
-        '-v',
-        'error',
-        '-show_format',
-        '-show_streams',
-        '-print_format',
-        'json',
-        filename,
-    ]
-    result = subprocess.run(
-        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-    )
-    if result.returncode != 0:
-        error_msg = result.stderr.decode('utf-8').strip()
-        raise Exception(error_msg)
-
-    output = result.stdout.decode('utf-8')
-    data = json.loads(output)
-
-    video_stream = next(
-        (
-            stream
-            for stream in data['streams']
-            if stream['codec_type'] == 'video'
-        ),
-        None,
-    )
-    if video_stream is None:
-        raise Exception("File has no video steam")
-
-    info = {
-        "width": int(video_stream.get('width', 0)),
-        "height": int(video_stream.get('height', 0)),
-        "duration": float(data['format']['duration']),
-        "orientation": int(
-            video_stream.get('side_data_list', [{}])[0].get('rotation', 0)
-        ),
-        "interlaced": (
-            video_stream.get('field_order', 'unknown') != "progressive"
-        ),
-    }
-    logging.info(f"{filename}: {info}")
-    return info
-
-
-FFMPEG_BINARY = "ffmpeg"
-FFMPEG_CODEC = "libx264"
-REENCODE_FPS = "25"
-
-
-def ffmpeg_apply_video_filters(in_file, out_file, filters, add_params=[]):
-    cmd = [FFMPEG_BINARY, "-y"]  # overwrite existing
-    cmd += ("-i", in_file)  # input file
-    cmd += ("-vf", ",".join(filters))  # filters
-    if out_file is None:
-        cmd += ("-f", "null", "-")
-    else:
-        cmd += ("-qp", "0")  # lossless
-        cmd += ("-preset", "ultrafast")  # maimum speed, big file
-        cmd += ("-acodec", "copy")  # copy audio as is
-        cmd += ("-c:v", FFMPEG_CODEC)  # video codec
-        cmd += add_params
-        cmd += (out_file,)
-    logging.debug(cmd)
-    res = subprocess.run(cmd).returncode
-    if res != 0:
-        raise Exception(f"ffmpeg_apply_video_filters failed: {res}")
-
-
-def ffmpeg_deinterlace(in_file, out_file):
-    filters = [
-        "yadif",
-        "format=yuv420p",
-    ]
-    logging.info("start deinterlace")
-    ffmpeg_apply_video_filters(in_file, out_file, filters)
-
-
-def ffmpeg_stabilize(in_file, out_file, tmpdirname):
-    trffile = os.path.join(tmpdirname, 'transforms.txt')
-    try:
-        filters = [
-            f"vidstabdetect=stepsize=32:shakiness=10:accuracy=10:result={trffile}",
-        ]
-        logging.info("start stab prep")
-        ffmpeg_apply_video_filters(in_file, None, filters)
-
-        filters = [
-            f"vidstabtransform=input={trffile}:zoom=0:smoothing=10,unsharp=5:5:0.8:3:3:0.4",
-        ]
-        logging.info("start stab")
-        ffmpeg_apply_video_filters(in_file, out_file, filters)
-    finally:
-        try:
-            os.unlink(trffile)
-        except FileNotFoundError:
-            pass
-
-
-def ffmpeg_resize(in_file, out_file, w, h):
-    filters = [
-        "format=yuv420p",
-        f"scale=w='if(gt(a,{w}/{h}),{w},trunc(oh*a/2)*2)':h='if(gt(a,{w}/{h}),trunc(ow/a/2)*2,{h})'",
-        f"pad={w}:{h}:(ow-iw)/2:(oh-ih)/2:black",
-    ]
-    add_params = ["-r", REENCODE_FPS]
-    logging.info("start resize")
-    ffmpeg_apply_video_filters(in_file, out_file, filters, add_params)
-
-
-def ffmpeg_concatenate(filenames, out_file, tmpdirname):
-    if len(filenames) == 0:
-        logging.warning("empty filenames list")
-        return
-    listfile = os.path.join(tmpdirname, 'list.txt')
-    with open(listfile, 'w') as f:
-        for fname in filenames:
-            f.write(f"file '{fname}'\n")
-
-    cmd = [FFMPEG_BINARY, "-y"]  # overwrite existing
-    cmd += ("-f", "concat")
-    cmd += ("-safe", "0")
-    cmd += ("-i", listfile)
-    cmd += ("-c:v", FFMPEG_CODEC)
-    cmd += ("-crf", "17")  # produce a visually lossless file.
-    cmd += ("-bf", "2")  # limit consecutive B-frames to 2
-    cmd += ("-use_editlist", "0")  # avoids writing edit lists
-    # places moov atom/box at front of the output file.
-    cmd += ("-movflags", "+faststart")
-    # use the native encoder to produce an AAC audio stream.
-    cmd += ("-c:a", "aac")
-    cmd += ("-q:a", "1")  # sets the highest quality for the audio.
-    cmd += ("-ac", "2")  # rematrixes audio to stereo.
-    cmd += ("-ar", "48000")  # resamples audio to 48000 Hz.
-    cmd += (out_file,)
-    logging.info("start concatenate")
-    logging.debug(cmd)
-    try:
-        res = subprocess.run(cmd).returncode
-        if res != 0:
-            raise Exception(f"concatenate failed: {res}")
-        logging.info(f"file saved: {out_file}")
-    finally:
-        try:
-            os.unlink(listfile)
-        except FileNotFoundError:
-            pass
-
-
-def concatenate_to_mp4(
-    filenames, outputfile, tmpdirname="/tmp", dry_run=False
-):
-    max_height = 0
-    max_width = 0
-    durations = []
-    interlaced = []
-    for f in filenames:
-        info = ffprobe_get_video_info(f)
-        durations.append(info["duration"])
-        interlaced.append(info["interlaced"])
-        w = info["width"]
-        h = info["height"]
-        if info["orientation"] not in (0, 180, -180):
-            w, h = h, w
-        if w > max_width:
-            max_width = w
-            max_height = h
-
-    logging.info(f"Result video: width={max_width}, height={max_height}")
-
-    tmpfilenames = []
-
-    if dry_run:
-        return durations
-
-    for i, f in enumerate(filenames):
-        fname = os.path.join(tmpdirname, f"{i}.mp4")
-        tfname = os.path.join(tmpdirname, f"{i}_tmp.mp4")
-
-        logging.info(f"convert '{f}' to '{fname}'")
-
-        if interlaced[i]:
-            ffmpeg_deinterlace(f, tfname)
-            os.rename(tfname, fname)
-            f = fname
-
-        ffmpeg_stabilize(f, tfname, tmpdirname)
-        os.rename(tfname, fname)
-
-        ffmpeg_resize(fname, tfname, max_width, max_height)
-        os.rename(tfname, fname)
-
-        tmpfilenames.append(fname)
-
-    ffmpeg_concatenate(tmpfilenames, outputfile, tmpdirname)
-
-    for f in tmpfilenames:
-        os.unlink(f)
-
-    return durations
 
 
 def get_youtube_credentials(client_secrets, token_file):
@@ -297,20 +93,12 @@ def generate_description(time_labels):
 class VideoProcessor(object):
     def __init__(self, update_existing):
         self.__update_existing = update_existing
-        scan_paths = list(
-            filter(
-                None,
-                os.getenv("VIDEO_LIB_ROOTS").split(":"),
-            ),
-        )
-        db_file = os.getenv("VIDEO_LIB_DB")
-
         self.__work_dir = os.getenv("VIDEO_PROCESSOR_WORK_DIR")
         os.makedirs(self.__work_dir, exist_ok=True)
         self.__res_dir = os.getenv("VIDEO_PROCESSOR_RES_DIR")
         os.makedirs(self.__res_dir, exist_ok=True)
 
-        self.__lib = datelib.DateLib(scan_paths, db_file)
+        self.__lib = datelib.DateLib()
         self.__credentials = get_youtube_credentials(
             "client_secrets.json", "token.json"
         )
@@ -351,18 +139,18 @@ class VideoProcessor(object):
         logging.info(f"found {len(fnames)} files")
 
         resfilename = os.path.join(self.__res_dir, f"{date}.mp4")
-        durations = concatenate_to_mp4(fnames, resfilename, self.__work_dir)
+        fileinfos = mixvideoconcat.concat(fnames, resfilename, self.__work_dir)
 
         time_labels = []
-        for af, duration in zip(afiles, durations):
+        for af, info in zip(afiles, fileinfos):
             time = af.prop().time().strftime("%H: %M: %S")
             caption = af.load_json()["caption"].strip()
-            time_labels.append((duration, f"[{time}] {caption}"))
+            time_labels.append((info["duration"], f"[{time}] {caption}"))
 
         logging.info(time_labels)
 
         # TODO: uncomment
-        self.upload_video(resfilename, date, time_labels)
+        # self.upload_video(resfilename, date, time_labels)
 
         logging.info(f"Done: {date}")
 
